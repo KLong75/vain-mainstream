9edba60be6178916bf1bb1852595ccd17767e2ed
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-11-15 21:40:07 -0600

    implement mui grid for album track display

--- src/components/AlbumTracks/index.js
+++ src/components/AlbumTracks/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
+import { Paper } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import pixelRecord from '../../assets/images/pixel_record.png';
 
 const AlbumTracks = () => {
@@ -23,20 +25,34 @@ const AlbumTracks = () => {
 
   return (
     <section id='album-tracks-section'>
+      <Box sx={{ flexGrow: 1 }}> 
+      <Grid container spacing={4} >
       {tracks.map((track) => (
-        <div key={track.title}>
-          <div>
-           
-            <h3 id='track-title'>{track.title}</h3>
-            <img src={pixelRecord} alt='pixelated vinyl record' className='pixel-record'></img>
-            <audio controls>
-            <source src={require(`../../assets/audio/${track.src}`)}></source>
-            </audio>
-           
-          </div>
-        </div>
-
+            
+            
+                <Grid xs={4} key={track.title}>
+                  <Paper elevation={3}>
+                  <Grid>
+                  <h3 id='track-title'>{track.title}</h3>
+                  </Grid>
+                  <Grid>
+                  <img src={pixelRecord} alt='pixelated vinyl record' className='pixel-record'></img>
+                  </Grid>
+                  <Grid>
+                  <audio controls className='album-tracks-audio-player'>
+                    <source src={require(`../../assets/audio/${track.src}`)}></source>
+                  </audio>
+                  </Grid>
+                  <Grid>
+                  <FileDownloadIcon />
+                  </Grid>
+                  </Paper>
+                </Grid>
+             
+            
       ))} 
+       </Grid>
+      </Box> 
     </section>
   );
 };
