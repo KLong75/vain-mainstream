d693753a446c0f82ff38a68bd4b51206b785bdbb
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-11-18 15:01:11 -0600

    create skeleton band images, positions, layout, animations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,7 @@ import { styled } from '@mui/material/styles';
 // import images
 import flyingV from '../../assets/images/blue_flying_v.png';
 import blackGuitar from '../../assets/images/black_rock_guitar.png';
+import mohawkSkull from '../../assets/images/mohawk_skull.png';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -28,10 +29,10 @@ const Header = (props) => {
       <Item elevation={12} className='header-paper'>
 
 
-      <Stack direction='row' justifyContent='center'>
-      <img src={flyingV} className='header-guitar' alt='flying v electric guitar'/>
-      <h1 className='headline font-effect-fire-animation'>Vain Mainstream</h1>
-      <img src={blackGuitar} className='header-guitar' alt='black electric guitar'/>
+      <Stack direction='row' justifyContent='center' spacing={6}>
+      <img src={mohawkSkull} className='header-mohawk-skull' alt='flying v electric guitar'/>
+      <h1 className='header-headline font-effect-fire-animation'>Vain Mainstream</h1>
+      <img src={mohawkSkull} className='header-mohawk-skull' alt='black electric guitar'/>
       </Stack>
       <Nav
         setCurrentPage={props.setCurrentPage}
--- src/components/TheMainstream/index.js
+++ src/components/TheMainstream/index.js
@@ -18,9 +18,16 @@ import 'react-h5-audio-player/lib/styles.css';
 // import boombox from '../../assets/images/pixel_boombox_cropped.png';
 import speakers from '../../assets/images/two_speakers.png';
 import worldOnFire from '../../assets/images/pixel_world_on_fire_cropped.png';
-import drums from '../../assets/images/pixel_drums_big.png';
+// import drums from '../../assets/images/pixel_drums_big.png';
+import drumSkeleton from '../../assets/images/pixel_drums_headless_drummer2.png';
 import mohawkSkull from '../../assets/images/mohawk_skull.png';
-import skeletonNoSkull from '../../assets/images/pixel_skeleton_no_head.png';
+// import skeletonNoSkull from '../../assets/images/pixel_skeleton_no_head.png';
+import guitaristNoArm from '../../assets/images/pixel_guitarist_no_arm.png';
+import guitaristArm from '../../assets/images/guitarist-arm.png'
+import pixelSkull from '../../assets/images/pixel_skull.png';
+import flyingV from '../../assets/images/blue_flying_v.png';
+import blackGuitar from '../../assets/images/black_rock_guitar.png';
+
 // import audio
 // import cultOfP from '../../assets/audio/cult_of_personality.mp3';
 // import dancingQ from '../../assets/audio/DQ_no_vox.mp3';
@@ -71,48 +78,130 @@ const Mainstream = () => {
         <Marquee speed={40} gradientWidth={200} gradientColor={[0, 0, 255]} className='now-playing-marquee'>
           <h2 className='marquee-text'>Now Playing on Mainstream Radio: "{randomTrack.title}" by Vain Mainstream </h2>
         </Marquee>
+
         <Box sx={{ flexGrow: 1 }}>
+          <Item className='mainstream-paper' elevation={12}>
 
-        
 
-        <Item className='mainstream-paper' elevation={12}>
 
-          <Grid container justifyContent='center'>
-            <img item src={worldOnFire} alt='pixelated burning earth'  id='world-on-fire' />
-          </Grid>
 
-          <Grid container justifyContent='center'>
-            <img item src={mohawkSkull} alt='pixelated skull with mohawk'  id='mohawk-skull' />
-          </Grid>
 
-          <Grid container justifyContent='center'>
-            <img item src={skeletonNoSkull} alt='skeleton body'  id='skeleton-drummer-body' />
-          </Grid>
 
           <Grid container spacing={1} disableEqualOverflow>
-            <Grid item xs={3} >
-              <img src={speakers} alt='pixelated loudspeakers' id='speakers-img' />
-            </Grid>
-            <Grid item xs={6} >
-              <img src={drums} alt='pixelated drumset' id='' className='drums' />
-            </Grid>
-            <Grid item xs={3} >
-            <img src={speakers} alt='pixelated loudspeakers' id='speakers-img' />
-            </Grid>
-          </Grid>
 
-          <Grid item>
-            {/* <audio id='music-player' controls loop >
-              <source src={require(`../../assets/audio/${randomTrack.src}`)} ></source>
-            </audio> */}
-            <AudioPlayer
-              item
-              autoPlay
-              loop 
-              className='react-h5-audio-player' 
-              src={require(`../../assets/audio/${randomTrack.src}`)} 
-            />
-          </Grid>
+<Grid item xs={3} >
+  <img src={pixelSkull} className='guitarist-skull' alt='flying v electric guitar'/>
+</Grid>
+
+<Grid item xs={6}>
+  <div></div>
+</Grid>
+
+<Grid item xs={3} >
+  <img src={pixelSkull} className='guitarist-skull' alt='black electric guitar'/>
+</Grid>
+
+</Grid>
+          
+
+
+
+
+
+
+
+
+
+
+            <Grid container spacing={1} disableEqualOverflow>
+
+              <Grid item xs={3} >
+                  <img src={guitaristNoArm} alt='pixelated skeleton' className='skeleton-guitarist' />
+              </Grid>
+
+
+              <Grid item xs={6}>
+                <img item src={worldOnFire} alt='pixelated burning earth'  id='world-on-fire' />
+              </Grid>
+
+              <Grid item xs={3} >
+                <img src={guitaristNoArm} alt='pixelated skeleton' className='skeleton-guitarist' />
+              </Grid>
+
+            </Grid>
+
+
+
+            <Grid container spacing={1} disableEqualOverflow>
+
+<Grid item xs={3} >
+  <img src={guitaristArm} className='guitarist-arm' alt='skeleton arm playing guitar'/>
+</Grid>
+
+<Grid item xs={6}>
+  <div></div>
+</Grid>
+
+<Grid item xs={3} >
+  <img src={guitaristArm} className='guitarist-arm' alt='skeleton arm playing guitar'/>
+</Grid>
+
+</Grid>
+
+
+
+
+
+            <Grid container spacing={1} disableEqualOverflow>
+
+              <Grid item xs={3} >
+                <img src={flyingV} className='mainstream-guitar' alt='flying v electric guitar'/>
+              </Grid>
+
+              <Grid item xs={6}>
+                <div></div>
+              </Grid>
+
+              <Grid item xs={3} >
+                <img src={blackGuitar} className='mainstream-guitar' alt='black electric guitar'/>
+              </Grid>
+
+            </Grid>
+
+
+            
+ 
+
+
+
+
+
+            <Grid container justifyContent='center'>
+             <img item src={mohawkSkull} alt='pixelated skull with mohawk'  id='mohawk-skull' />
+            </Grid>
+
+
+            <Grid container spacing={1} disableEqualOverflow>
+              <Grid item xs={3} >
+                <img src={speakers} alt='pixelated loudspeakers' id='speakers-img' />
+              </Grid>
+              <Grid item xs={6} >
+                <img src={drumSkeleton} alt='pixelated drumset' id='' className='drums' />
+              </Grid>
+              <Grid item xs={3} >
+              <img src={speakers} alt='pixelated loudspeakers' id='speakers-img' />
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <AudioPlayer
+                item
+                autoPlay='false'
+                // loop 
+                className='react-h5-audio-player' 
+                src={require(`../../assets/audio/${randomTrack.src}`)} 
+              />
+            </Grid>
           </Item>
         </Box>
         
