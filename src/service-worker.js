c1c818e15ea5d307ea9dffa3ca96f6971412c47e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-01-29 14:05:22 -0600

    add even listener to service worker to load fresh content after updates

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,11 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+
+// take below out if something breaks
+self.addEventListener('visibilitychange', function() {
+  if (document.visibilityState === 'visible') {
+      console.log('APP resumed');
+      window.location.reload();
+  }
+});
