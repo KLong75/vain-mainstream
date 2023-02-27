af9cd48ebc454c339949e75902c74e6976354730
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-02-26 21:14:39 -0600

    adjust guitar spacing

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -17,7 +17,7 @@ const Album = () => {
   return (
     <section id='album-div'>
       <Grid container justifyContent='center'>
-        <Grid xs={12} justifyContent='center'>
+        <Grid xs={12} >
           <h2 id='album-headline' className='font-effect-fire-animation'>Your Likeness</h2>
         </Grid>
       </Grid>
@@ -34,23 +34,6 @@ const Album = () => {
           </a>
         </Grid>
       </Grid>
-      
-      {/* <Marquee speed={40} gradientWidth={100} gradientColor={[0, 0, 255]} className='tour-marquee'>
-          <span 
-            className='album-marquee-text'> 
-              Download tracks from the new album "Your Likeness" by Vain Mainstream today! A new track will be added each week of the Life Expectancy Tour until all 25 tracks have been revealed.
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </span>
-          <span 
-            className='album-marquee-text'> 
-              <a href='https://www.patreon.com/vainmainstream' target='_blank' rel="noreferrer" className='marquee-patreon-link'>
-                The album is free but medical bills are not. Please click to give me a dollar for health insurance.
-              </a>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </span>
-      </Marquee> */}
       
       <AlbumTracks />
     </section>
